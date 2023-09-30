a36bc3e348aca95727425c142f7a72d45a0a8bd6
PauloLuedy <pauloluedyads@gmail.com>
2023-09-30 04:54:17 -0300

    refatoração

---
+++ src/modules/users/resolvers.users.ts
@@ -0,0 +1,19 @@
+import { Inject } from '@nestjs/common';
+import {
+  Query,
+  Resolver,
+} from '@nestjs/graphql';
+import 'reflect-metadata';
+import { PrismaService } from '../../prisma.service';
+
+@Resolver()
+export class UserResolver {
+  constructor(@Inject(PrismaService) private prismaService: PrismaService) { }
+
+  @Query()
+  async allUsers() {
+    return this.prismaService.user.findMany()
+  }
+}
+
+
